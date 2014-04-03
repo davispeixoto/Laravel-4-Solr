b566e3c197f0f599aa7e56dc309db91afd7c03a0
Davis L P Peixoto <davis.peixoto@gmail.com>
2014-04-03 10:25:20 -0300

    initial commit

---
+++ src/Davispeixoto/Laravel4Solr/Laravel4SolrServiceProvider.php
@@ -0,0 +1,44 @@
+<?php namespace Davispeixoto\Laravel4Solr;
+
+use Illuminate\Support\ServiceProvider;
+
+class Laravel4SolrServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = false;
+
+	/**
+	 * Bootstrap the application events.
+	 *
+	 * @return void
+	 */
+	public function boot()
+	{
+		$this->package('davispeixoto/laravel4-solr');
+	}
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
